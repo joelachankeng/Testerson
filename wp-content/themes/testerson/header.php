32a8725d849e5b6733794965c8deb529d167c30c
joelachankeng <47440462+joelachankeng@users.noreply.github.com>
2023-03-01 11:04:38 -0500

    developed header element

--- wp-content/themes/testerson/header.php
+++ wp-content/themes/testerson/header.php
@@ -9,6 +9,8 @@
  *
  * @package TestersonTheme
  */
+$custom_logo_id = get_theme_mod('custom_logo');
+$image = wp_get_attachment_image_src($custom_logo_id, 'full');
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
@@ -26,7 +28,7 @@
     <!-- google font - montserrat-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <?php wp_head(); ?>
 
@@ -36,3 +38,72 @@
 
     <div id="page" class="site">
         <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'TestersonTheme'); ?></a>
+        <header class="main-header">
+            <nav class="top-navigation bg-bg-darkGrey py-3">
+                <div class="container mx-auto">
+                    <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'menu-top',
+                        'menu_class' => "flex justify-end items-center text-white gap-11",
+                    ));
+
+                    ?>
+                </div>
+            </nav>
+            <nav class="main-navigation">
+                <div class="container mx-auto mt-9 flex items-center justify-between">
+                    <div>
+                        <a href="/" class="logo-container flex items-start">
+                            <img class=" h-16 w-16 object-contain object-top" src="<?php echo $image[0]; ?>" alt="<?php echo get_bloginfo('name'); ?>">
+                            <div class="site-title flex flex-col">
+                                <p class="m-0 font-extrabold text-5xl lead">Testerson</p>
+                                <span class="tagline text-xl leading-6 uppercase text-end -mt-2 ">Printing</span>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="main-menu flex items-center justify-between gap-16 ">
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'menu-main',
+                            'menu_class' => "flex justify-end items-center text-white gap-16",
+                        ));
+
+                        ?>
+                        <div class="search-button-container">
+                            <input type="text" id="header-search-field" class="hidden">
+                            <button class="search-button">
+                                Search
+                            </button>
+                        </div>
+                        <div class="mobile-menu-actions hidden">
+                            <ul class="mobile-actions">
+                                <li id="mobile-search-item">
+                                    <label for="mobile-search">Search</label>
+                                    <button id="mobile-search"></button>
+                                </li>
+                                <li id="mobile-toggle-item">
+                                    <label for="mobile-toggle">Menu</label>
+                                    <div id="mobile-toggle" class="">
+                                        <span></span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="mobile-menu hidden">
+                    <div class="container mx-auto">
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'menu-main',
+                            'menu_class' => "flex justify-end items-center",
+                            'container_class' => 'menu-mobile-main-menu-container'
+                        ));
+
+                        ?>
+                    </div>
+
+                </div>
+            </nav>
+        </header>
